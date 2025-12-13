ecde57e4f1b51b0668cdbfb327738f6c87fe88b6
HasanDibas1 <92920719+HasanDibas1@users.noreply.github.com>
2025-12-13 22:11:10 +0300

    Update index.js

--- src/api/GetRoles/index.js
+++ src/api/GetRoles/index.js
@@ -1,1 +1,21 @@
 
+module.exports = async function (context, req) {
+    // Get authenticated user info
+    const clientPrincipal = req.headers["x-ms-client-principal"]
+        ? JSON.parse(Buffer.from(req.headers["x-ms-client-principal"], "base64").toString("utf8"))
+        : null;
+
+    // Default: no roles
+    let roles = [];
+
+    if (clientPrincipal) {
+        roles = clientPrincipal.userRoles || [];
+    }
+
+    context.res = {
+        body: {
+            roles: roles
+        }
+    };
+};
+
